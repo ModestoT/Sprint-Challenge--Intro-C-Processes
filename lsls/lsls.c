3cf33ecc760a1b3ac7b263f61c85c4ce91b30af4
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-31 12:32:12 -0500

    made it to where the full path is being sent to the stat() function call. Allows getting more accurate file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * Main
  */
@@ -9,7 +11,6 @@ int main(int argc, char **argv)
   struct stat buf;
   struct dirent *ent;
   DIR *dir;
-
   // Parse command line
   if (argc < 2){
     dir = opendir(".");
@@ -25,7 +26,7 @@ int main(int argc, char **argv)
   } else {
     //   Call opendir().
     dir = opendir(argv[1]);
-
+    
     if (dir == NULL){
       printf("Cannot open directory '%s'\n", argv[1]);
       return 1;
@@ -33,9 +34,14 @@ int main(int argc, char **argv)
 
     //   Then repeatedly call readdir()--printing the filenames as you go--until it lets you know there are no more directory entries by returning NULL.
     while((ent = readdir(dir)) != NULL){
-      stat(ent->d_name, &buf);
+      char *path = malloc(strlen(argv[1]) + strlen(ent->d_name) + 2);
+      sprintf(path, "%s/%s", argv[1], ent->d_name);
+
+      stat(path, &buf);
       printf("%ld ", buf.st_size);
       printf("%s\n", ent->d_name);
+
+      free(path);
     }
     //   Then call closedir().
     closedir(dir);
