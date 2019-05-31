cd0f1b0b3fc8a6f89539d7a26f032c0a3255d171
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-31 10:49:37 -0500

    completed the first test case of user not inputting any directory and printing what is in the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,24 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *ent;
+  DIR *dir;
   // Parse command line
+  if (argc < 2){
+    dir = opendir(".");
+    
+    while((ent = readdir(dir)) != NULL){
+      printf("%s\n", ent->d_name);
+    }   
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+    closedir(dir);
+  } else {
+  //   Call opendir().
+  dir = opendir(argv[1]);
+  //   Then repeatedly call readdir()--printing the filenames as you go--until it lets you know there are no more directory entries by returning NULL.
+  //   Then call closedir().
+  closedir(dir);
+  }
 
   return 0;
 }
