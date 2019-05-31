d431467cad187199435e3b44fee8e4b8512cac10
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-31 10:55:36 -0500

    completed listing out the files in the specified directory the user inputs

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -18,11 +18,20 @@ int main(int argc, char **argv)
 
     closedir(dir);
   } else {
-  //   Call opendir().
-  dir = opendir(argv[1]);
-  //   Then repeatedly call readdir()--printing the filenames as you go--until it lets you know there are no more directory entries by returning NULL.
-  //   Then call closedir().
-  closedir(dir);
+    //   Call opendir().
+    dir = opendir(argv[1]);
+
+    if (dir == NULL){
+      printf("Cannot open directory '%s'\n", argv[1]);
+      return 1;
+    }
+
+    //   Then repeatedly call readdir()--printing the filenames as you go--until it lets you know there are no more directory entries by returning NULL.
+    while((ent = readdir(dir)) != NULL){
+      printf("%s\n", ent->d_name);
+    }
+    //   Then call closedir().
+    closedir(dir);
   }
 
   return 0;
