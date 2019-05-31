ef101da62110e270cfeca6a7fd5e58aec4cbefd9
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-31 11:08:29 -0500

    figured out how to get the size of the files for the base case of no directory being given

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <sys/stat.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  struct stat buf;
   struct dirent *ent;
   DIR *dir;
+
   // Parse command line
   if (argc < 2){
     dir = opendir(".");
     
     while((ent = readdir(dir)) != NULL){
+      // printf("Example path: ./%s\n", ent->d_name);
+      stat(ent->d_name, &buf);
+      printf("File size: %ld ", buf.st_size);
       printf("%s\n", ent->d_name);
     }   
 
